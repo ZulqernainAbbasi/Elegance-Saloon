cc39390e1aba6479fee65cbe768349b51d75edd9
ZulqernainAbbasi <160768841+ZulqernainAbbasi@users.noreply.github.com>
2026-01-20 21:39:49 +0500

    Update email.php

--- Back End/email.php
+++ Back End/email.php
@@ -49,12 +49,12 @@ class User extends Database {
             $mail->isSMTP();
             $mail->Host = 'smtp.gmail.com';
             $mail->SMTPAuth = true;
-            $mail->Username = 'student1498744.aptechiic@gmail.com';
-            $mail->Password = 'hqgroekedjoulohs';
+            $mail->Username = '';
+            $mail->Password = '';
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port = 587;
 
-            $mail->setFrom('student1498744.aptechiic@gmail.com', 'Aptechiic');
+            $mail->setFrom('', '');
             $mail->addAddress($email);
 
             $mail->isHTML(true);
